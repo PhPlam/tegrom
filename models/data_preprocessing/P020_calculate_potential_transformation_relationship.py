5bd84c7bc6a52731002e1d07bd29907ece93290e
PhPlam <plamper.p+git@gmail.com>
2023-03-09 10:13:43 +0100

    (work linux) update function to calc relationships

--- models/data_preprocessing/P020_calculate_potential_transformation_relationship.py
+++ models/data_preprocessing/P020_calculate_potential_transformation_relationship.py
@@ -1,5 +1,5 @@
 # Name: Philipp Plamper
-# Date: 24. january 2023
+# Date: 09. march 2023
 
 import pandas as pd
 from progress.bar import Bar
@@ -9,117 +9,103 @@ import P000_path_variables_preprocess as pvp
 #calculate possible transformations###############################################
 ##################################################################################
 
-# calculate new molecules from given molecules and transformation units in direction: photoaddition
-def calculate_new_formulas_photoaddition(formula_strings, transformation_unit):
+# calculate new molecules from given molecules and transformation units
+def calculate_formula_relationships(formula_strings, transformation_unit):
+    # create empty list to store dictionaries with calculated relationships
     formula_list = []
     # initiate progress bar
-    bar_calculate_new_formulas_photoaddition = Bar('calculate new formulas photoaddition:', max = len(formula_strings.index))
+    bar_calculate_relationships = Bar('calculate relationships:', max = len(formula_strings.index))
 
+    # iterate through dataframe of molecular formulas
     for row1 in formula_strings.itertuples():
-        bar_calculate_new_formulas_photoaddition.next()
-        # file with all transformation_units
+        bar_calculate_relationships.next()
+        # iterate through dataframe of transformation units
         for row2 in transformation_unit.itertuples():
-            # choose if transformation is useful in this model
-            if row2.plus == 0:
-                continue
+            # calculate new molecules from given molecules and transformation units in direction: photoaddition
+            # check if transformation unit exist as photo addition in data
+            if int(row2.plus) == 1:
+                # define exist as True
+                # if molecule of calculated relationship has atom < 0 
+                # jumps to False and code skips the relationship
+                exist = True
+                # create empty dictionary for new formulas
+                formula_dict = {}
+                # initiate atoms per formula with 0
+                new_C = new_H = new_O = new_N = new_S = 0
 
-            # create empty dictionary for new formulas
-            formula_dict = {}
-            # initiate atoms per formula with 0
-            new_C = new_H = new_O = new_N = new_S = 0
+                # calculate possible new molecule
+                # calculation: formula - transformation_unit = new_formula
+                # skip if count < 0
+                new_C = row1.C - row2.C if row1.C - row2.C >= 0 else -1
+                if new_C == -1: exist = False
+                new_H = row1.H - row2.H if row1.H - row2.H >= 0 else -1
+                if new_H == -1: exist = False
+                new_O = row1.O - row2.O if row1.O - row2.O >= 0 else -1
+                if new_O == -1: exist = False
+                new_N = row1.N - row2.N if row1.N - row2.N >= 0 else -1
+                if new_N == -1: exist = False
+                new_S = row1.S - row2.S if row1.S - row2.S >= 0 else -1
+                if new_S == -1: exist = False
 
-            # calculate possible new molecule
-            # calculation: formula - transformation_unit = new_formula
-            # skip if count < 0
-            new_C = row1.C - row2.C if row1.C - row2.C >= 0 else -1
-            if new_C == -1: continue
-            new_H = row1.H - row2.H if row1.H - row2.H >= 0 else -1
-            if new_H == -1: continue
-            new_O = row1.O - row2.O if row1.O - row2.O >= 0 else -1
-            if new_O == -1: continue
-            new_N = row1.N - row2.N if row1.N - row2.N >= 0 else -1
-            if new_N == -1: continue
-            new_S = row1.S - row2.S if row1.S - row2.S >= 0 else -1
-            if new_S == -1: continue
+                # create dictionary with results
+                # only if calculated molecule can exist
+                if exist == True:
+                    formula_dict = {
+                        'formula_string': row1.formula_string,
+                        'transformation_unit': row2.element,
+                        'is_addition' : 1,
+                        'new_C': new_C,
+                        'new_H': new_H,
+                        'new_O': new_O,
+                        'new_N': new_N,
+                        'new_S': new_S,
+                        'tu_C': row2.C,
+                        'tu_H': row2.H,
+                        'tu_O': row2.O,
+                        'tu_N': row2.N,
+                        'tu_S': row2.S
+                    }
+                    # append dictionary to list
+                    formula_list.append(formula_dict)
 
-            # create dictionary with results
-            formula_dict = {
-                'formula_string': row1.formula_string,
-                'transformation_unit': row2.element,
-                'is_addition' : 1,
-                'new_C': new_C,
-                'new_H': new_H,
-                'new_O': new_O,
-                'new_N': new_N,
-                'new_S': new_S,
-                'tu_C': row2.C,
-                'tu_H': row2.H,
-                'tu_O': row2.O,
-                'tu_N': row2.N,
-                'tu_S': row2.S
-            }
-            # append dictionary to list
-            formula_list.append(formula_dict)
+            # calculate new molecules from given molecules and transformation units in direction: photodegradation
+            # choose if transformation unit exist as photo elimination in data
+            if int(row2.minus) == 1:
+                # create empty dictionary for new formulas
+                formula_dict = {}
+                # initiate atoms per formula with 0
+                new_C = new_H = new_O = new_N = new_S = 0
 
-    bar_calculate_new_formulas_photoaddition.finish()
+                # calculate possible new atom count in molecule
+                # calculation: formula + transformation_unit = new_formula
+                new_C = row1.C + row2.C
+                new_H = row1.H + row2.H
+                new_O = row1.O + row2.O
+                new_N = row1.N + row2.N
+                new_S = row1.S + row2.S
+
+                # create dictionary with results
+                formula_dict = {
+                    'formula_string': row1.formula_string,
+                    'transformation_unit': row2.element,
+                    'is_addition': 0,
+                    'new_C': new_C,
+                    'new_H': new_H,
+                    'new_O': new_O,
+                    'new_N': new_N,
+                    'new_S': new_S,
+                    'tu_C': (-1)*row2.C if row2.C != 0 else 0,
+                    'tu_H': (-1)*row2.H if row2.H != 0 else 0,
+                    'tu_O': (-1)*row2.O if row2.O != 0 else 0,
+                    'tu_N': (-1)*row2.N if row2.N != 0 else 0,
+                    'tu_S': (-1)*row2.S if row2.S != 0 else 0
+                }
+                # append dictionary to list
+                formula_list.append(formula_dict)
+
+    bar_calculate_relationships.finish()
     print('done: calculate new molecules photoaddition')
     return formula_list
-
-# calculate new molecules from given molecules and transformation units in direction: photodegradation
-def calculate_new_formulas_photodegradation(formula_strings, transformation_unit):
-    formula_list = []
-    # initiate progress bar
-    bar_calculate_new_formulas_photodegradation = Bar('calculate new formulas photodegradation:', max = len(formula_strings.index))
-
-    for row1 in formula_strings.itertuples():
-        bar_calculate_new_formulas_photodegradation.next()
-        # file with all transformation_units
-        for row2 in transformation_unit.itertuples():
-            # choose if transformation is useful in this model
-            if row2.minus == 0:
-                continue
-
-            # create empty dictionary for new formulas
-            formula_dict = {}
-            # initiate atoms per formula with 0
-            new_C = new_H = new_O = new_N = new_S = 0
-
-            # calculate possible new atom count in molecule
-            # calculation: formula + transformation_unit = new_formula
-            new_C = row1.C + row2.C
-            new_H = row1.H + row2.H
-            new_O = row1.O + row2.O
-            new_N = row1.N + row2.N
-            new_S = row1.S + row2.S
-
-            # create dictionary with results
-            formula_dict = {
-                'formula_string': row1.formula_string,
-                'transformation_unit': row2.element,
-                'is_addition': 0,
-                'new_C': new_C,
-                'new_H': new_H,
-                'new_O': new_O,
-                'new_N': new_N,
-                'new_S': new_S,
-                'tu_C': (-1)*row2.C if row2.C != 0 else 0,
-                'tu_H': (-1)*row2.H if row2.H != 0 else 0,
-                'tu_O': (-1)*row2.O if row2.O != 0 else 0,
-                'tu_N': (-1)*row2.N if row2.N != 0 else 0,
-                'tu_S': (-1)*row2.S if row2.S != 0 else 0
-            }
-            # append dictionary to list
-            formula_list.append(formula_dict)
-
-    bar_calculate_new_formulas_photodegradation.finish()
-    print('done: calculate new molecules photodegradation')
-    return formula_list
-
-# concatenate calculated lists of molecules
-def merge_calculated_molecules(calculated_photoaddtion, calculated_photodegradation):
-    merged_list = calculated_photoaddtion + calculated_photodegradation
-    print('done: concatenate both calculated lists')
-    return merged_list
 
 # create strings from calculated molecules
 def create_strings_from_molecules(merged_molecule_list):
@@ -174,7 +160,7 @@ def check_existence_of_strings(df_added_strings):
 def create_strings_transformation_unit(df_molecules):
     tu_list = []
     # initiate progress bar
-    bar_create_strings_transformation_units = Bar('check existence:', max = len(df_molecules.index))
+    bar_create_strings_transformation_units = Bar('create strings of transformation units:', max = len(df_molecules.index))
 
     for row in df_molecules.itertuples(): # index=False
         bar_create_strings_transformation_units.next()
@@ -220,11 +206,8 @@ if __name__ == '__main__':
     transformation_unit = pvp.load_csv(pvp.file_transformation_units, seperator=[',', ';'])
 
     # calculate
-    calculated_photoaddtion = calculate_new_formulas_photoaddition(formula_strings, transformation_unit)
-    calculated_photodegradation = calculate_new_formulas_photodegradation(formula_strings, transformation_unit)
-
-    merged_molecule_list = merge_calculated_molecules(calculated_photoaddtion, calculated_photodegradation)
-    df_added_strings = create_strings_from_molecules(merged_molecule_list)
+    calculated_relationships = calculate_formula_relationships(formula_strings, transformation_unit)
+    df_added_strings = create_strings_from_molecules(calculated_relationships)
     df_molecules = check_existence_of_strings(df_added_strings)
     calculated_transformations = create_strings_transformation_unit(df_molecules)
 
