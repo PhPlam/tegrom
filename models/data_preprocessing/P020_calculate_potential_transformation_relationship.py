62d1a5355f69735d548da9e7e2ba5f77afb1aedd
PhPlam <plamper.p+git@gmail.com>
2023-03-09 13:45:19 +0100

    (work linux) improve calculation of relationships

--- models/data_preprocessing/P020_calculate_potential_transformation_relationship.py
+++ models/data_preprocessing/P020_calculate_potential_transformation_relationship.py
@@ -17,13 +17,13 @@ def calculate_formula_relationships(formula_strings, transformation_unit):
     bar_calculate_relationships = Bar('calculate relationships:', max = len(formula_strings.index))
 
     # iterate through dataframe of molecular formulas
-    for row1 in formula_strings.itertuples():
+    for mol_formula in formula_strings.itertuples():
         bar_calculate_relationships.next()
         # iterate through dataframe of transformation units
-        for row2 in transformation_unit.itertuples():
+        for trans_unit in transformation_unit.itertuples():
             # calculate new molecules from given molecules and transformation units in direction: photoaddition
             # check if transformation unit exist as photo addition in data
-            if int(row2.plus) == 1:
+            if int(trans_unit.plus) == 1:
                 # define exist as True
                 # if molecule of calculated relationship has atom < 0 
                 # jumps to False and code skips the relationship
@@ -36,41 +36,46 @@ def calculate_formula_relationships(formula_strings, transformation_unit):
                 # calculate possible new molecule
                 # calculation: formula - transformation_unit = new_formula
                 # skip if count < 0
-                new_C = row1.C - row2.C if row1.C - row2.C >= 0 else -1
+                new_C = mol_formula.C - trans_unit.C if mol_formula.C - trans_unit.C >= 0 else -1
                 if new_C == -1: exist = False
-                new_H = row1.H - row2.H if row1.H - row2.H >= 0 else -1
+                new_H = mol_formula.H - trans_unit.H if mol_formula.H - trans_unit.H >= 0 else -1
                 if new_H == -1: exist = False
-                new_O = row1.O - row2.O if row1.O - row2.O >= 0 else -1
+                new_O = mol_formula.O - trans_unit.O if mol_formula.O - trans_unit.O >= 0 else -1
                 if new_O == -1: exist = False
-                new_N = row1.N - row2.N if row1.N - row2.N >= 0 else -1
+                new_N = mol_formula.N - trans_unit.N if mol_formula.N - trans_unit.N >= 0 else -1
                 if new_N == -1: exist = False
-                new_S = row1.S - row2.S if row1.S - row2.S >= 0 else -1
+                new_S = mol_formula.S - trans_unit.S if mol_formula.S - trans_unit.S >= 0 else -1
                 if new_S == -1: exist = False
 
                 # create dictionary with results
                 # only if calculated molecule can exist
                 if exist == True:
                     formula_dict = {
-                        'formula_string': row1.formula_string,
-                        'transformation_unit': row2.element,
+                        'formula_string': mol_formula.formula_string,
+                        'transformation_unit': trans_unit.element,
                         'is_addition' : 1,
                         'new_C': new_C,
                         'new_H': new_H,
                         'new_O': new_O,
                         'new_N': new_N,
                         'new_S': new_S,
-                        'tu_C': row2.C,
-                        'tu_H': row2.H,
-                        'tu_O': row2.O,
-                        'tu_N': row2.N,
-                        'tu_S': row2.S
+                        'tu_C': trans_unit.C,
+                        'tu_H': trans_unit.H,
+                        'tu_O': trans_unit.O,
+                        'tu_N': trans_unit.N,
+                        'tu_S': trans_unit.S
                     }
-                    # append dictionary to list
-                    formula_list.append(formula_dict)
+                    formula_dict['new_formula'] = create_string_from_calculated_atoms(formula_dict)
+                    # check if calculated molecule exists in original molecule data
+                    if formula_dict['new_formula'] in formula_strings['formula_string'].values:
+                        # create strings from atoms of transformation units
+                        formula_dict['transformation_unit'] = create_strings_transformation_unit(formula_dict)
+                        # append dictionary to list
+                        formula_list.append(formula_dict)
 
             # calculate new molecules from given molecules and transformation units in direction: photodegradation
             # choose if transformation unit exist as photo elimination in data
-            if int(row2.minus) == 1:
+            if int(trans_unit.minus) == 1:
                 # create empty dictionary for new formulas
                 formula_dict = {}
                 # initiate atoms per formula with 0
@@ -78,122 +83,82 @@ def calculate_formula_relationships(formula_strings, transformation_unit):
 
                 # calculate possible new atom count in molecule
                 # calculation: formula + transformation_unit = new_formula
-                new_C = row1.C + row2.C
-                new_H = row1.H + row2.H
-                new_O = row1.O + row2.O
-                new_N = row1.N + row2.N
-                new_S = row1.S + row2.S
+                new_C = mol_formula.C + trans_unit.C
+                new_H = mol_formula.H + trans_unit.H
+                new_O = mol_formula.O + trans_unit.O
+                new_N = mol_formula.N + trans_unit.N
+                new_S = mol_formula.S + trans_unit.S
 
                 # create dictionary with results
                 formula_dict = {
-                    'formula_string': row1.formula_string,
-                    'transformation_unit': row2.element,
+                    'formula_string': mol_formula.formula_string,
+                    'transformation_unit': trans_unit.element,
                     'is_addition': 0,
                     'new_C': new_C,
                     'new_H': new_H,
                     'new_O': new_O,
                     'new_N': new_N,
                     'new_S': new_S,
-                    'tu_C': (-1)*row2.C if row2.C != 0 else 0,
-                    'tu_H': (-1)*row2.H if row2.H != 0 else 0,
-                    'tu_O': (-1)*row2.O if row2.O != 0 else 0,
-                    'tu_N': (-1)*row2.N if row2.N != 0 else 0,
-                    'tu_S': (-1)*row2.S if row2.S != 0 else 0
+                    'tu_C': (-1)*trans_unit.C if trans_unit.C != 0 else 0,
+                    'tu_H': (-1)*trans_unit.H if trans_unit.H != 0 else 0,
+                    'tu_O': (-1)*trans_unit.O if trans_unit.O != 0 else 0,
+                    'tu_N': (-1)*trans_unit.N if trans_unit.N != 0 else 0,
+                    'tu_S': (-1)*trans_unit.S if trans_unit.S != 0 else 0
                 }
-                # append dictionary to list
-                formula_list.append(formula_dict)
+                formula_dict['new_formula'] = create_string_from_calculated_atoms(formula_dict)
+                # check if calculated molecule exists in original molecule data
+                if formula_dict['new_formula'] in formula_strings['formula_string'].values:
+                    # create strings from atoms of transformation units
+                    formula_dict['transformation_unit'] = create_strings_transformation_unit(formula_dict)
+                    # append dictionary to list
+                    formula_list.append(formula_dict)
 
     bar_calculate_relationships.finish()
+    molecule_df = pd.DataFrame(formula_list)
     print('done: calculate new molecules photoaddition')
-    return formula_list
+    return molecule_df
 
-# create strings from calculated molecules
-def create_strings_from_molecules(merged_molecule_list):
-    molecule_df = pd.DataFrame(merged_molecule_list)
-    help_list = []
-    # initiate progress bar
-    bar_build_strings = Bar('build strings from molecules:', max = len(molecule_df.index))
-
-    # take calculated atoms and append to string -> structure: 'CHNOS'
-    for row in molecule_df.itertuples():
-        bar_build_strings.next()
-        new_molecule = ''
-        if row.new_C > 0: new_molecule = 'C' + str(row.new_C)
-        if row.new_H > 0: new_molecule = new_molecule + ' H' + str(row.new_H)
-        if row.new_N > 0: new_molecule = new_molecule + ' N' + str(row.new_N)
-        if row.new_O > 0: new_molecule = new_molecule + ' O' + str(row.new_O)
-        if row.new_S > 0: new_molecule = new_molecule + ' S' + str(row.new_S)
-
-        # append new formula string to list
-        help_list.append(new_molecule)
+# create strings from calculated atom count
+def create_string_from_calculated_atoms(formula_dict):
     
-    # append new formula strings to dataframe
-    molecule_df['new_formula'] = help_list
-    bar_build_strings.finish()
-    print('done: create strings from molecules')
-    return molecule_df
+    # take calculated atoms and append to string -> structure: 'CHNOS'
+    new_molecule = ''
+    if formula_dict['new_C'] > 0: new_molecule = 'C' + str(formula_dict['new_C'])
+    if formula_dict['new_H'] > 0: new_molecule = new_molecule + ' H' + str(formula_dict['new_H'])
+    if formula_dict['new_N'] > 0: new_molecule = new_molecule + ' N' + str(formula_dict['new_N'])
+    if formula_dict['new_O'] > 0: new_molecule = new_molecule + ' O' + str(formula_dict['new_O'])
+    if formula_dict['new_S'] > 0: new_molecule = new_molecule + ' S' + str(formula_dict['new_S'])
 
-# check existence of strings in the molecule data
-def check_existence_of_strings(df_added_strings):
-    occurence_list = []
-    # initiate progress bar
-    bar_check_existence = Bar('check existence:', max = len(df_added_strings.index))
-
-    # check if molecule exist in molecule data
-    for row in df_added_strings.itertuples():
-        bar_check_existence.next()
-        if row.new_formula in formula_strings['formula_string'].values:
-            occurence_list.append(1)
-        else:
-            occurence_list.append(0)
-
-    bar_check_existence.finish()
-
-    df_added_strings['occur'] = occurence_list
-    # drop rows with molecules that does not exist
-    df_added_strings.drop(df_added_strings[df_added_strings.occur == 0].index, inplace=True)
-    molecule_df = df_added_strings.drop(columns=['occur'])
-    print('done: check existence of molecules')
-    return molecule_df
+    return new_molecule
 
 # create strings from atoms of transformation units
-def create_strings_transformation_unit(df_molecules):
-    tu_list = []
-    # initiate progress bar
-    bar_create_strings_transformation_units = Bar('create strings of transformation units:', max = len(df_molecules.index))
+def create_strings_transformation_unit(formula_dict):
 
-    for row in df_molecules.itertuples(): # index=False
-        bar_create_strings_transformation_units.next()
-        tu_string = ""
+    tu_string = ""
 
-        if row.tu_C < 0: tu_string = "-" + tu_string + "C" + str(abs(row.tu_C)) + " "
-        elif row.tu_C > 0: tu_string = tu_string + "C" + str(row.tu_C) + " "
-        else: tu_string
+    if formula_dict['tu_C'] < 0: tu_string = "-" + tu_string + "C" + str(abs(formula_dict['tu_C'])) + " "
+    elif formula_dict['tu_C'] > 0: tu_string = tu_string + "C" + str(formula_dict['tu_C']) + " "
+    else: tu_string
 
-        if row.tu_H < 0: tu_string = tu_string + "-H" + str(abs(row.tu_H)) + " "
-        elif row.tu_H > 0: tu_string = tu_string + "H" + str(row.tu_H) + " "
-        else: tu_string
+    if formula_dict['tu_H'] < 0: tu_string = tu_string + "-H" + str(abs(formula_dict['tu_H'])) + " "
+    elif formula_dict['tu_H'] > 0: tu_string = tu_string + "H" + str(formula_dict['tu_H']) + " "
+    else: tu_string
 
-        if row.tu_O < 0: tu_string = tu_string + "-O" + str(abs(row.tu_O)) + " "
-        elif row.tu_O > 0: tu_string = tu_string + "O" + str(row.tu_O) + " "
-        else: tu_string
+    if formula_dict['tu_O'] < 0: tu_string = tu_string + "-O" + str(abs(formula_dict['tu_O'])) + " "
+    elif formula_dict['tu_O'] > 0: tu_string = tu_string + "O" + str(formula_dict['tu_O']) + " "
+    else: tu_string
 
-        if row.tu_N < 0: tu_string = tu_string + "-N" + str(abs(row.tu_N)) + " "
-        elif row.tu_N > 0: tu_string = tu_string + "N" + str(row.tu_N) + " "
-        else: tu_string
+    if formula_dict['tu_N'] < 0: tu_string = tu_string + "-N" + str(abs(formula_dict['tu_N'])) + " "
+    elif formula_dict['tu_N'] > 0: tu_string = tu_string + "N" + str(formula_dict['tu_N']) + " "
+    else: tu_string
 
-        if row.tu_S < 0: tu_string = tu_string + "-S" + str(abs(row.tu_S)) + " "
-        elif row.tu_S > 0: tu_string = tu_string + "S" + str(row.tu_S) + " "
-        else: tu_string
+    if formula_dict['tu_S'] < 0: tu_string = tu_string + "-S" + str(abs(formula_dict['tu_S'])) + " "
+    elif formula_dict['tu_S'] > 0: tu_string = tu_string + "S" + str(formula_dict['tu_S']) + " "
+    else: tu_string
 
-        # print(tu_string)
-        tu_string = tu_string.rstrip()
-        tu_list.append(tu_string)
+    tu_string = tu_string.rstrip()
 
-    bar_create_strings_transformation_units.finish()    
-    df_molecules['transformation_unit'] = tu_list
-    print('done: create strings for transformation unit')
-    return df_molecules
+    return tu_string
 
 ##################################################################################
 #call functions###################################################################
@@ -207,9 +172,6 @@ if __name__ == '__main__':
 
     # calculate
     calculated_relationships = calculate_formula_relationships(formula_strings, transformation_unit)
-    df_added_strings = create_strings_from_molecules(calculated_relationships)
-    df_molecules = check_existence_of_strings(df_added_strings)
-    calculated_transformations = create_strings_transformation_unit(df_molecules)
 
     #export
-    pvp.export_csv(pvp.relationships, calculated_transformations)
+    pvp.export_csv(pvp.relationships, calculated_relationships)
