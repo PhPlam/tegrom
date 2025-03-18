209d3817f3dc97de52c74c1c13d84f49eee295cf
PhPlam <plamper.p+git@gmail.com>
2025-03-18 10:53:45 +0100

    update

---
+++ models/variables/V003_remote_conf.py
@@ -0,0 +1,11 @@
+# Name: Philipp Plamper
+# Date: 25. june 2024
+
+# pushed to project from "EXECUTE_TEGROM/configuration.txt" as "V003_remote_conf.py
+
+###names for neo4j databases_____________________________
+db_name_temporal = 'modeltemporal' # name of temporal graph, see naming conventions in 'instructions.md', default: modeltemporal
+
+###name of files_________________________________________
+file_molecules = 'molecule_formulas.csv' # assigned molecular formulas by mass spectrometry, default: molecule_formulas.csv
+file_transformation_units = 'transformations_handwritten.csv' # transformation units, default: transformations_handwritten.csv
